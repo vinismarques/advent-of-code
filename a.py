e19ed8fead52e6685ea4010bb39ac099cd219914
Vinícius Marques <vinicius_marques@live.com>
2025-12-04 23:27:45 -0300

    init

---
+++ a.py
@@ -0,0 +1,13 @@
+from aocd import get_data
+from dotenv import load_dotenv
+
+
+def main() -> None:
+    data = get_data(day=1, year=2025)
+
+    print("Hello from adventofcode! Data:", data)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
